4e4eeb97b6e3d56d1f0f789f8540c1d4b207b247
chaituveer <chaithanyaveeraiahgari@gmail.com>
2020-06-25 22:03:46 -0500

    rub analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,56 +1,51 @@
-library(dplyr)
+# load libraries
+library(dplyr) 
 
-###Reading Activity files
-ActTest <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/Y_test.txt", col.names = "code")
-ActTrain <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/Y_train.txt", col.names = "code")
+# set dataset directory
+##setwd("UCI HAR Dataset")
 
-###Read features files
-FeaTest <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-FeaTrain <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+# read train data 
+x_train   <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/X_train.txt")
+y_train   <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/Y_train.txt") 
+sub_train <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/subject_train.txt")
 
-#Read subject files
-SubTest <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/subject_test.txt",  col.names = "subject")
-SubTrain <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+# read test data 
+x_test   <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/X_test.txt")
+y_test   <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/Y_test.txt") 
+sub_test <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/test/subject_test.txt")
 
-####Read Activity Labels
-ActLabels <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/activity_labels.txt",col.names = c("code", "activity"))
+# read features description 
+features <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/features.txt") 
 
-#####Read Feature Names
-FeaNames <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+# read activity labels 
+activity_labels <- read.table("/Users/chaitu/Downloads/UCI HAR Dataset/activity_labels.txt") 
 
-#####Merg dataframes: Features Test&Train,Activity Test&Train, Subject Test&Train
-FeaData <- rbind(FeaTest, FeaTrain)
-SubData <- rbind(SubTest, SubTrain)
-ActData <- rbind(ActTest, ActTrain)
-Totaldata<- cbind(FeaData, SubData, ActData)
+# merge of training and test sets
+x_total   <- rbind(x_train, x_test)
+y_total   <- rbind(y_train, y_test) 
+sub_total <- rbind(sub_train, sub_test) 
+
+# keep only measurements for mean and standard deviation 
+sel_features <- variable_names[grep(".*mean\\(\\)|std\\(\\)", features[,2], ignore.case = FALSE),]
+x_total      <- x_total[,sel_features[,1]]
+
+# name columns
+colnames(x_total)   <- sel_features[,2]
+colnames(y_total)   <- "activity"
+colnames(sub_total) <- "subject"
+
+# merge final dataset
+total <- cbind(sub_total, y_total, x_total)
+
+# turn activities & subjects into factors 
+total$activity <- factor(total$activity, levels = activity_labels[,1], labels = activity_labels[,2]) 
+total$subject  <- as.factor(total$subject) 
+
+# create a summary independent tidy dataset from final dataset 
+# with the average of each variable for each activity and each subject. 
+total_mean <- total %>% group_by(activity, subject) %>% summarize_all(funs(mean)) 
 
 
+# export summary dataset
+write.table(total_mean, file = "/Users/chaitu/Downloads/UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE) 
 
-####Create Tidy Data set with the average of each variable
-
-TidyData <- Totaldata %>% select(subject, code, contains("mean"), contains("std"))
-
-####Create a second, independent tidy data set with activity namea
-
-TidyData$code <- activities[TidyData$code, 2]
-
-##### Allocate names to tidy data
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
-
-#Save this tidy dataset to local file
-FinalData <- TidyData %>%
-  group_by(subject, activity) %>%
-  summarise_all(funs(mean))
-write.table(FinalData, "tidydata.txt", row.name=FALSE)
